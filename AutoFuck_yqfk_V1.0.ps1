ec34e065768c5fef4f7c98990e82aea229745b5c
RenegadeHRH <49357198+RenegadeHRH@users.noreply.github.com>
2022-01-07 09:40:30 +0800

    Update AutoFuck_yqfk_V1.0.ps1

--- AutoFuck_yqfk_V1.0.ps1
+++ AutoFuck_yqfk_V1.0.ps1
@@ -1,6 +1,6 @@
 
 param($username,$psw)
-
+echo $username.$psw
 <#------获取PHPSESSID------#>
 $session = New-Object Microsoft.PowerShell.Commands.WebRequestSession
 $session.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 Edg/96.0.1054.62"
