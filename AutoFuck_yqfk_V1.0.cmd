98ddedd47dcaeac1d91d3a54a4b391bdb89a8792
RenegadeHRH <49357198+RenegadeHRH@users.noreply.github.com>
2022-02-16 03:03:25 +0800

    Update and rename AutoFuck_yqfk_V1.0.cmd to AutoFuck_yqfk_V1.1.cmd

--- AutoFuck_yqfk_V1.0.cmd
+++
@@ -1,1 +0,0 @@
-start powershell  -WindowStyle Hidden -Command "AutoFuck_yqfk_V1.0.ps1" -username 账号 -psw 密码
---
+++ AutoFuck_yqfk_V1.1.cmd
@@ -0,0 +1,1 @@
+start powershell  -WindowStyle Hidden -Command "AutoFuck_yqfk_V1.1.ps1" -username 账号 -psw 密码
